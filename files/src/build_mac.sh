eaf5d6104c19b2eab81da98fcdb9c2a2ed19dcdd
C. R. Zamana <czamana@gmail.com>
2026-05-30 22:28:36 -0300

    Fix electron version, add build config and mac build script

---
+++ files/src/build_mac.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#npx electron-builder --mac
+
+# Para arm64
+npm run build:mac-arm64  # alias para electron-builder --mac --arm64
+
+# Para x64
+npm run build:mac-x64   # alias para electron-builder --mac --x64
+
